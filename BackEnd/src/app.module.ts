ae6461ea8ec23b9a18e46a5c8a0c02bda1363882
Jackson <jacksonaparesido@gmail.com>
2024-09-01 03:22:57 -0300

    Criação do user

---
+++ BackEnd/src/app.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { TarefaModule } from './tarefa/tarefa.module';
+
+@Module({
+  imports: [UserModule, TarefaModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
---
+++ BackEnd/src/tarefa/dto/update-tarefa.dto.ts
@@ -0,0 +1,3 @@
+import { PartialType } from '@nestjs/mapped-types';
+import { CreateTarefaDto } from './create-tarefa.dto';
+export class UpdateTarefaDto extends PartialType(CreateTarefaDto) {}
